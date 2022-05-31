b784873ad986f5fa7af13b276df6745bf40c6e77
Allan Rodrigues <93384872+AllanRodrigues2021@users.noreply.github.com>
2022-05-31 15:25:06 -0300

    Create CoxinhaDeBueno.java

---
+++ Desafios/CoxinhaDeBueno.java
@@ -0,0 +1,20 @@
+package me.dio.desafios.desafiodecodigo;
+
+import java.util.Scanner;
+
+public class DesafioCoxinhaDoBueno {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        //System.out.print("Digite,respectivamente, o número de coxinhas consumidas e o número de participantes: ");
+        String input = scan.nextLine();
+
+        String[] inputSplit = input.split(" ");
+        String H = inputSplit[0];
+        String P = inputSplit[1];
+
+        double media = Double.parseDouble(H) / Double.parseDouble(P);
+
+        System.out.printf("%.2f", media);
+    }
+}
