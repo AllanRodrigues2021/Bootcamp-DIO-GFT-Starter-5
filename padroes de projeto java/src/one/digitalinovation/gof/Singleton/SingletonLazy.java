97facb4985189541cbb1ee7277954c1a3562eec2
Allan Rodrigues <93384872+AllanRodrigues2021@users.noreply.github.com>
2022-05-31 20:46:06 -0300

    Create SingletonLazy.java

---
+++ padroes de projeto java/src/one/digitalinovation/gof/Singleton/SingletonLazy.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.gof.singleton;
+
+
+public class SingletonLazy {
+
+    private static SingletonLazy instancia;
+
+    //construtor privado de forma que outras classes não possam instanciar a mesma diretamente
+    private SingletonLazy(){
+        super();
+    }
+
+    //para chamar a instancia é necessário utilizar o método abaixo, que somente cria uma instancia da classe se ela ainda não tiver sido criada.
+    public static SingletonLazy getInstancia(){
+        if (instancia==null){
+            instancia = new SingletonLazy();
+        }
+        return instancia;
+    }
+}
