7315a9ba582325ea00e62eb58a290a250d6a874a
Allan Rodrigues <93384872+AllanRodrigues2021@users.noreply.github.com>
2022-05-31 15:28:22 -0300

    Create NumeroReverso.java

---
+++ Desafios/Live Numero Reverso/NumeroReverso.java
@@ -0,0 +1,17 @@
+package me.dio.desafios.livecamila;
+
+import java.util.Scanner;
+
+public class NumeroReverso {
+
+    public static void numeroReverso() {
+
+        Scanner input = new Scanner(System.in);
+/*
+        do {
+            System.out.println("Digite um número inteiro e positivo de 4 digitos: ");
+        } while (true);
+        }
+*/
+    }
+}
